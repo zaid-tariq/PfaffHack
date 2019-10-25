342b739beae6fa7af6529750cc70a38e0d78ebcd
Syed.Samad Shakeel <shakeel@rhrk.uni-kl.de>
2019-10-25 18:38:55 +0200

    asd

--- js/fuctions.ts
+++ js/fuctions.ts
@@ -58,5 +58,13 @@ class Ride {
 
 class Trip {
     id: number;
-    
+    driver: number;
+    passenger: number;
+    rideId: number;
+
+    constructor(driver: number, passenger: number, rideId: number){
+        this.driver = driver;
+        this.passenger = passenger;
+        this.rideId = rideId;
+    }
 }
