64b80f0f830c501592ce3414b82117218e257493
Syed.Samad Shakeel <shakeel@rhrk.uni-kl.de>
2019-10-25 20:41:28 +0200

    sf

--- src/user.ts
+++ src/user.ts
@@ -19,5 +19,29 @@ class User {
     Sign_up(firstName: string, lastName: string, username: string, password: string, contact: string, email: string){
         let user = new User(firstName, lastName, username, password, contact, email);
 
+        var apiData = {
+            "type": "create-user",
+            "sender": "A-Team",
+            "payload": {
+                "username": user.username,
+                "firstName": user.firstName,
+                "lastName": user.lastName,
+                "password": user.password,
+                "contact": user.contact,
+                "email": user.email
+            },
+            "tags": [
+                "user", "account"
+            ]
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "http://194.94.239.125:9000",
+            data: apiData,
+            success: success,
+            dataType: dataType
+        })  
+
     }
 }
