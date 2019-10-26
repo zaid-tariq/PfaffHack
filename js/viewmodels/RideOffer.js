b3873d9f78d655757d1cd337f20c952e045918f7
Zaid <mohammadzaidtariq@gmail.com>
2019-10-26 02:34:15 +0200

    push

--- js/viewmodels/RideOffer.js
+++ js/viewmodels/RideOffer.js
@@ -1,65 +1,66 @@
 define([], function() {
-    function VM() {
-      var self = this;
-      self.myName = ko.observable("Ride Offer");
-      self.username = ko.observable();
-      self.firstName = ko.observable();
-      self.lastName = ko.observable();
-      self.searchQuery = ko.observable("");
-      self.addressSuggestions = ko.observableArray();
-      self.selectedAddress = ko.observable();
-      self.APP_ID = "WpwySCOwyFoixH4fFs0B"
-      self.APP_CODE = "SDh1tnEiV0cj1ZYtojznQA"
-      self.Query = "Pariser+1+Berl"
+  function VM() {
+    var self = this;
+    self.myName = ko.observable("Ride Offer");
+    self.username = ko.observable();
+    self.firstName = ko.observable();
+    self.lastName = ko.observable();
+    self.searchQuery = ko.observable("");
+    self.addressSuggestions = ko.observableArray();
+    self.selectedAddress = ko.observable();
+    self.APP_ID = "WpwySCOwyFoixH4fFs0B";
+    self.APP_CODE = "SDh1tnEiV0cj1ZYtojznQA";
+    self.Query = "Pariser+1+Berl";
 
-      self.onLoad = function() {          
-      };
+    self.onLoad = function() {};
 
-      self.searchAddress = function(){
-        url = "http://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id="+self.APP_ID+"&app_code="+self.APP_CODE+
-          "&query="+self.searchQuery()+"&beginHighlight=<b>&endHighlight=</b>"
-        $.ajax({
-            url: url,
-            success: function(res){
-                console.info(res)
-                self.addressSuggestions(res["suggestions"])
-            },
-            error: function(res){
-                console.info("ERROR")
-                console.info(res)
-            }
-          });
-
-          
-      }
-
-      self.chooseThisPlace = function(point){
-          console.info(point)
-          self.selectedAddress(point)
-          console.info(self.selectedAddress())
-      }
-  
-      self.reset = function() {
-        // alert("reset");
-      };
-  
-      self.onExit = function() {
-        // alert("onexit");
-        self.reset();
-      };
-  
-      self.submit = function() {
-          
-        console.info(self.firstName());
-        console.info(self.lastName());
-        console.info(self.username());
-      };
-    }
-  
-    return {
-      getInstance: function() {
-        return new VM();
-      }
+    self.searchAddress = function() {
+      url =
+        "http://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=" +
+        self.APP_ID +
+        "&app_code=" +
+        self.APP_CODE +
+        "&query=" +
+        self.searchQuery() +
+        "&beginHighlight=<b>&endHighlight=</b>";
+      $.ajax({
+        url: url,
+        success: function(res) {
+          console.info(res);
+          self.addressSuggestions(res["suggestions"]);
+        },
+        error: function(res) {
+          console.info("ERROR");
+          console.info(res);
+        }
+      });
     };
-  });
-  
+
+    self.chooseThisPlace = function(point) {
+      console.info(point);
+      self.selectedAddress(point);
+      console.info(self.selectedAddress());
+    };
+
+    self.reset = function() {
+      // alert("reset");
+    };
+
+    self.onExit = function() {
+      // alert("onexit");
+      self.reset();
+    };
+
+    self.submit = function() {
+      console.info(self.firstName());
+      console.info(self.lastName());
+      console.info(self.username());
+    };
+  }
+
+  return {
+    getInstance: function() {
+      return new VM();
+    }
+  };
+});
