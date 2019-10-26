f5e9955c52e310210f110bc83114a673e04d9862
Syed.Samad Shakeel <shakeel@rhrk.uni-kl.de>
2019-10-26 06:11:57 +0200

    s

--- js/main.js
+++ js/main.js
@@ -1,79 +1,67 @@
-require(["config/config"], function(){
+require(["config/config"], function() {
+  require(["binder", "globals"], function(loader) {
+    loader.loadBaseView(true);
 
-	require(["binder", "globals"], function(loader){
+    Sammy(function() {
+      //Catch all possible patterns of hash.
+      this.get(/\#(.*)/, function() {
+        var allParams = this.params.splat[0].split("/");
+        var url = allParams[0].toLowerCase();
 
-		loader.loadBaseView(true)		
+        switch (url) {
+          case "Config".toLowerCase():
+            loader.loadConfigurator();
+            break;
 
-		Sammy(function() {
-		
-			//Catch all possible patterns of hash.
-			this.get(/\#(.*)/, function() {
-				
-				var allParams = this.params.splat[0].split("/");
-				var url = allParams[0].toLowerCase();
+          case "CreateBom".toLowerCase():
+            g_BaseVM.showBomPage();
+            break;
 
-				switch(url){
+          case "CreateRules".toLowerCase():
+            g_BaseVM.showRulesPage();
+            break;
 
-					case "Config".toLowerCase():
-						loader.loadConfigurator()
-						break;
+          case "SignUp".toLowerCase():
+            g_BaseVM.showSingUpPage();
+            break;
 
-					case "CreateBom".toLowerCase():
-						g_BaseVM.showBomPage()
-						break;
+          case "SignIn".toLowerCase():
+            g_BaseVM.showSignInPage();
+            break;
 
-					case "CreateRules".toLowerCase():
-						g_BaseVM.showRulesPage()
-						break;
-					
-					case "SignUp".toLowerCase():
-						g_BaseVM.showSingUpPage()
-						break;
-					
-					case "SignIn".toLowerCase():
-						g_BaseVM.showSignInPage()
-						break;
-					
-					case "RideOffer".toLowerCase():
-						g_BaseVM.showRideOfferPage()
-						break;
-						
-					case "RideSearch".toLowerCase():
-						g_BaseVM.showRideSearchPage()
-						break;
+          case "RideOffer".toLowerCase():
+            g_BaseVM.showRideOfferPage();
+            break;
 
-					case "UserSearchGivenEndPoints".toLowerCase():
-						g_BaseVM.showUserSearchGivenEndPointspage()
-						break;
+          case "RideSearch".toLowerCase():
+            g_BaseVM.showRideSearchPage();
+            break;
 
-					case "UpcomingRides".toLowerCase():
-						g_BaseVM.showUpcomingRidespage()
-						break;
+          case "UserSearchGivenEndPoints".toLowerCase():
+            g_BaseVM.showUserSearchGivenEndPointspage();
+            break;
 
-					case "RideReview".toLowerCase():
-						g_BaseVM.showRideReviewpage()
-						break;
+          case "UpcomingRides".toLowerCase():
+            g_BaseVM.showUpcomingRidespage();
+            break;
 
-					case "RideRequests".toLowerCase():
-						g_BaseVM.showRideRequestspage()
-						break;
+          case "RideReview".toLowerCase():
+            g_BaseVM.showRideReviewpage();
+            break;
 
-					case "Admin".toLowerCase():
-					case "Home".toLowerCase():
-						if(g_BaseVM)
-							g_BaseVM.showLandingPage()
-						break;
+          case "RideRequests".toLowerCase():
+            g_BaseVM.showRideRequestspage();
+            break;
 
-					default:
-						alert("The URL doesn't exist.")
-				}
-				
+          case "Admin".toLowerCase():
+          case "Home".toLowerCase():
+            if (g_BaseVM) g_BaseVM.showLandingPage();
+            break;
 
-	        });
-	        
-	    }).run();
-
-	})
-
+          default:
+            alert("The URL doesn't exist.");
+        }
+      });
+    }).run();
+  });
 });
-
