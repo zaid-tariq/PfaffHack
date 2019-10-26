8e19636a06e9fbe9d9fa0f89c05623b4644a621e
Zaid <mohammadzaidtariq@gmail.com>
2019-10-26 02:29:22 +0200

    commit 2

--- js/viewmodels/RideOffer.js
+++ js/viewmodels/RideOffer.js
@@ -5,8 +5,39 @@ define([], function() {
       self.username = ko.observable();
       self.firstName = ko.observable();
       self.lastName = ko.observable();
-  
-      self.onLoad = function() {};
+      self.searchQuery = ko.observable("");
+      self.addressSuggestions = ko.observableArray();
+      self.selectedAddress = ko.observable();
+      self.APP_ID = "WpwySCOwyFoixH4fFs0B"
+      self.APP_CODE = "SDh1tnEiV0cj1ZYtojznQA"
+      self.Query = "Pariser+1+Berl"
+
+      self.onLoad = function() {          
+      };
+
+      self.searchAddress = function(){
+        url = "http://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id="+self.APP_ID+"&app_code="+self.APP_CODE+
+          "&query="+self.searchQuery()+"&beginHighlight=<b>&endHighlight=</b>"
+        $.ajax({
+            url: url,
+            success: function(res){
+                console.info(res)
+                self.addressSuggestions(res["suggestions"])
+            },
+            error: function(res){
+                console.info("ERROR")
+                console.info(res)
+            }
+          });
+
+          
+      }
+
+      self.chooseThisPlace = function(point){
+          console.info(point)
+          self.selectedAddress(point)
+          console.info(self.selectedAddress())
+      }
   
       self.reset = function() {
         // alert("reset");
